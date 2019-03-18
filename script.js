722bca0ea7d0f6fc331eef8c43fbf5fd68da2894
Ky Guillaume <kyguillaume@MacBook-Air-de-Ky.local>
2019-03-18 10:55:40 +0100

    flowers 1 & 2

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function greet(){
 	window.alert("I'm the king!");
 }
 
-document.getElementById("first").src = "flower2.png"; 
-document.getElementById("container").getElementsByTagName("img")[1].src = "flower2.png";
-document.getElementById("container").getElementsByTagName("img")[3].src = "flower2.png";
+function init(){
+	document.getElementById("first").src = "flower2.png"; 
+	document.getElementById("container").getElementsByTagName("img")[1].src = "flower2.png";
+	document.getElementById("container").getElementsByTagName("img")[3].src = "flower2.png";
+}
